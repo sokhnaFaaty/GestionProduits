9ec12aa437bfa2ba248d8444680a50c043a50d36
makeda <adm670in@gmail.com>
2026-05-12 23:18:11 +0000

    modif index

--- public/index.php
+++ public/index.php
@@ -1,9 +1,10 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-define("ROOT", (substr($_SERVER['DOCUMENT_ROOT'] ,0, -6)));
+define("ROOT", (str_replace("public","",$_SERVER['DOCUMENT_ROOT'])));
 
-
+// var_dump($_SERVER['DOCUMENT_ROOT']);
+// var_dump(ROOT);
 define("WEBROOT","http://localhost:8003/");
 require_once(ROOT."views/header.php");
 require_once(ROOT."db/config.php");
