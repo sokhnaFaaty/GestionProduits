2c18f92395059eb1c4a5bf8827e1f99457bc301e
nexuusdev <mamefat2004@gmail.com>
2026-05-20 03:18:42 +0200

    commit

--- controllers/commandeController.php
+++ controllers/commandeController.php
@@ -1,5 +1,39 @@
+
 <?php
 require_once(ROOT . "models/commandeModel.php");
+require_once(ROOT . "models/ligneCommandeModel.php");
+require_once(ROOT . "models/clientModel.php");
+require_once(ROOT . "models/produitModel.php");
 
+$pages = ["listeCommande", "detailCommande", "ajoutCommande", "traiterAjoutCommande"];
 
-$pages=["ajoutCommande","listeCommande","detailCommande"];
+function listeCommande() {
+    $commandes = getAllCommandes();
+    require_once(ROOT . "views/commandes/listeCommande.php");
+}
+
+function ajoutCommande() {
+    $errors   = [];
+    $clients  = getAllClients();
+    $produits = getAllProduits();
+
+    if (isset($_REQUEST['envoie'])) {
+        $code      = $_POST['code'];
+        $date      = $_POST['date'];
+        $id_client = $_POST['id_client'];
+        $id_produits = $_POST['id_produit'] ?? [];
+        $quantites   = $_POST['quantite']   ?? [];
+        $prix        = $_POST['prix']        ?? [];
+
+        if (empty($code))      $errors['code']      = "Le code est obligatoire";
+        if (empty($date))      $errors['date']      = "La date est obligatoire";
+        if (empty($id_client)) $errors['id_client'] = "Choisissez un client";
+        if (empty($id_produits)) $errors['produits'] = "Ajoutez au moins un produit";
+
+      
+    }
+
+    require_once(ROOT . "views/commandes/ajoutCommande.php");
+}
+
+0000000
