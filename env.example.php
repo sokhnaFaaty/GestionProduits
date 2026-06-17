4b170f3d7a493374f2f5f1bfce2449f74e05247a
nexuusdev <mamefat2004@gmail.com>
2026-06-17 09:48:42 +0200

    ajoutr des fichichiers env

---
+++ env.example.php
@@ -0,0 +1,7 @@
+<?php
+define('DB_HOST',     'votre_nom_d_hote');
+define('DB_PORT',     'votre_numero_de_port');
+define('DB_NAME',     'votre_nom_de_base_de_donnee');
+define('DB_USERNAME', 'votre_nom_d_utilisateur');
+define('DB_PASSWORD', 'votre_mot_de_passe');
+define('APP_URL',     'https://votre-domaine.alwaysdata.net/');
