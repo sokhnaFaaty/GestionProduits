0323420cad2379b0597b446fb0858d718bb67d5c
nexuusdev <mamefat2004@gmail.com>
2026-06-18 03:15:37 +0200

    remove env.dev.php from git tracking

--- env.dev.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('DB_HOST',     '127.0.0.1');
-define('DB_PORT',     '5432');
-define('DB_NAME',     'ProduitGestiongroupeFaty');
-define('DB_USERNAME', 'postgres');
-define('DB_PASSWORD', 'ubuntu');
-define('APP_URL',     'http://localhost:8003/');
